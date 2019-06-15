aa38bf235720f74c0ea536fb254de8d09450c267
openchung <42964002+openchung@users.noreply.github.com>
2019-06-15 10:12:50 +0800

    Update App.java

--- src/main/java/com/mycompany/helloworld/App.java
+++ src/main/java/com/mycompany/helloworld/App.java
@@ -11,6 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello to myself!" );
         System.out.println( "Finishing CI Course" );
-        System.out.println( "Add test" );
+        System.out.println( "Add docker" );
     }
 }
